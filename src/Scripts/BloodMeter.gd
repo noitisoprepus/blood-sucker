105604a41bea863aff635440b544be0aec1c72af
noitisoprepus <ralphryan.peralta@gmail.com>
2022-11-06 00:14:57 +0800

    Fixed blood meter's unreponsiveness * Made the vial progress texture size to be exact * Changed the tweening code of the blood meter * Changed the color, opacity, and offset of the clouds texture

--- src/Scripts/BloodMeter.gd
+++ src/Scripts/BloodMeter.gd
@@ -1,20 +1,21 @@
 extends Control
 
 onready var meter = $TextureProgress
-onready var player = get_parent().get_parent().get_node("Player")
+onready var tween = $Tween
 
 
 func _ready():
 	meter.value = 0
+	var player = get_parent().get_parent().get_node("Player")
 	player.connect("drank_blood", self, "fill_blood")
 	player.connect("death", self, "empty_vial")
 
 
 func fill_blood(amount: float) -> void:
-	var tween := create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
-	tween.tween_property(meter, "value", amount, 1.0)
+	tween.interpolate_property(meter, "value", meter.value, amount, 1.0, Tween.TRANS_EXPO, Tween.EASE_OUT)
+	tween.start()
 
 
 func empty_vial() -> void:
-	var tween := create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(meter, "value", 0.0, 2.0)
+	tween.interpolate_property(meter, "value", meter.value, 0, 1.0, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	tween.start()
